f2d61e2b7e8882ff19e64a4df209f45727edf0ed
pietrohenr <ph5103417@gmail.com>
2024-07-01 17:16:11 -0300

    portfolio

--- assets/js/api.js
+++ assets/js/api.js
@@ -1,7 +1,7 @@
 
 
 async function fetchProfileData() {
-    const url = 'https://github.com/vcr1985/Meu-Portfolio';
+    const url = 'https://raw.githubusercontent.com/vcr1985/Meu-Portfolio/main/assets/js/api.js';
     const response = await fetch(url)
     const profileData = await response.json()
     return profileData
